6b33c568dbafe1d36cf8126926c4fd5b16870fcb
Anoop <optimistanoop@gmil.com>
2015-12-02 17:36:43 +0530

    initial commit

---
+++ app/src/test/java/project0/nanodegree/anoop/com/nanodegree_project_0_my_app_portfolio/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package project0.nanodegree.anoop.com.nanodegree_project_0_my_app_portfolio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
