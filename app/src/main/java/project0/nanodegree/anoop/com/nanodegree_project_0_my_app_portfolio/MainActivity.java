aa465ae30c337f114d70d84158a22b3107703f0f
Anoop <optimistanoop@gmil.com>
2015-12-08 10:57:34 +0530

    Final changes according to doc

--- app/src/main/java/project0/nanodegree/anoop/com/nanodegree_project_0_my_app_portfolio/MainActivity.java
+++ app/src/main/java/project0/nanodegree/anoop/com/nanodegree_project_0_my_app_portfolio/MainActivity.java
@@ -1,13 +1,14 @@
 package project0.nanodegree.anoop.com.nanodegree_project_0_my_app_portfolio;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +18,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button buttonSpotifyStreamer = (Button) this.findViewById(R.id.spotifyStreamer);
+        buttonSpotifyStreamer.setOnClickListener(new ClickListener());
+        Button buttonBuildItBigger = (Button) this.findViewById(R.id.buildItBigger);
+        buttonBuildItBigger.setOnClickListener(new ClickListener());
+        Button buttonCapstone = (Button) this.findViewById(R.id.capstone);
+        buttonCapstone.setOnClickListener(new ClickListener());
+        Button buttonLibraryApp = (Button) this.findViewById(R.id.libraryApp);
+        buttonLibraryApp.setOnClickListener(new ClickListener());
+        Button buttonScoresApp = (Button) this.findViewById(R.id.scoresApp);
+        buttonScoresApp.setOnClickListener(new ClickListener());
+        Button buttonXYZReader = (Button) this.findViewById(R.id.xYZReader);
+        buttonXYZReader.setOnClickListener(new ClickListener());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+   private class ClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String text  = ((Button)v).getText().toString();
+            if( !(text.endsWith("App"))){
+             text=   text.concat(" App");
+            };
+            showToast("This button will launch my " + text.toLowerCase()+ "!");
+        }
+        public void showToast(String message){
+            Context context = getApplicationContext();
+            CharSequence text = message;
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
