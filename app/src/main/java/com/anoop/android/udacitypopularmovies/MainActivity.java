010e5b54853828cf70fe9f9220b152c7412639c7
Anoop <optimistanoop@gmail.com>
2016-03-12 19:19:20 +0530

    movies grid fragment added with settings menu on main activity

--- app/src/main/java/com/anoop/android/udacitypopularmovies/MainActivity.java
+++ app/src/main/java/com/anoop/android/udacitypopularmovies/MainActivity.java
@@ -1,13 +1,41 @@
 package com.anoop.android.udacitypopularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements MovieGridFragment.OnFragmentInteractionListener{
+
+    /*TODO develop for tabs
+    * cross check requirement
+    * test it thoroughly
+    * use comments and logs
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
