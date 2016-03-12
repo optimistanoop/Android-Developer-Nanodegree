9c9c2d403a5b4d15dd7c6545c1159eb05d517cd2
Anoop <optimistanoop@gmail.com>
2016-03-12 22:43:24 +0530

    movie and movies data model added for apis

---
+++ app/src/main/java/com/anoop/android/udacitypopularmovies/Movie.java
@@ -0,0 +1,129 @@
+package com.anoop.android.udacitypopularmovies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anoop on 12/3/16.
+ */
+public class Movie {
+
+    private Boolean adult;
+    private String backdropPath;
+    private List<Integer> genreIds = new ArrayList<Integer>();
+    private Integer id;
+    private String language;
+    private String overview;
+    private String releaseDate;
+    private String posterLink;
+    private Double popularity;
+    private String title;
+    private Boolean video;
+    private Double voteAverage;
+    private Integer voteCount;
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPosterLink() {
+        return posterLink;
+    }
+
+    public void setPosterLink(String posterLink) {
+        this.posterLink = posterLink;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+}
+
