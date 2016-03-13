5e81cc19bb7bc6f05bdfc8418ca5340d371de90d
Anoop <optimistanoop@gmail.com>
2016-03-13 18:00:20 +0530

    constants added

--- app/src/main/java/com/anoop/android/udacitypopularmovies/Constants.java
+++ app/src/main/java/com/anoop/android/udacitypopularmovies/Constants.java
@@ -4,4 +4,18 @@ package com.anoop.android.udacitypopularmovies;
  * Created by anoop on 13/3/16.
  */
 public class Constants {
+    public static final String ID="id";
+    public static final String ADULT = "adult";
+    public static final String BACKDROP_PATH = "backdrop_path";
+    public static final String OVERVIEW = "overview";
+    public static final String RELEASE_DATE = "release_date";
+    public static final String POSTER_LINK = "poster_path";
+    public static final String POPULARITY= "popularity";
+    public static final String TITLE = "title";
+    public static final String VOTE_AVERAGE = "vote_average";
+    public static final String VOTE_COUNT = "vote_count";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String API_KEY = BuildConfig.MOVIE_DB_API_KEY;
+    public static final String API_PARAM = "api_key";
+    public static final String JSON_RESULTS = "results";
 }
