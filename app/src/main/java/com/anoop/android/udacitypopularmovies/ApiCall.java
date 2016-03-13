f071416ff5280e789e413c572f182626cd2daaf8
Anoop <optimistanoop@gmail.com>
2016-03-13 18:02:15 +0530

    async task for movies api written

--- app/src/main/java/com/anoop/android/udacitypopularmovies/ApiCall.java
+++ app/src/main/java/com/anoop/android/udacitypopularmovies/ApiCall.java
@@ -1,7 +1,109 @@
 package com.anoop.android.udacitypopularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * Created by anoop on 13/3/16.
  */
-public class ApiCall {
+public class ApiCall extends AsyncTask<String, Void, ArrayList<Movie>> {
+
+
+    private MoviesApiCallResults MoviesApiCallResultsCallback;
+
+    public ApiCall(MoviesApiCallResults callback) {
+        MoviesApiCallResultsCallback = callback;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Movie> movies) {
+        super.onPostExecute(movies);
+        if (MoviesApiCallResultsCallback != null) {
+            MoviesApiCallResultsCallback.moviesApiCallResultsCallback(movies);
+        }
+    }
+
+    protected ArrayList<Movie> doInBackground(String... params) {
+        if (MoviesApiCallResultsCallback == null) {
+            return null;
+        }
+
+        HttpURLConnection httpURLConnection = null;
+        BufferedReader bufferedReader = null;
+
+        String jsonResults;
+
+        try {
+            Uri.Builder builder = Uri.parse("http://api.themoviedb.org/3/discover/movie").buildUpon();
+            builder.appendQueryParameter(Constants.API_KEY, Constants.API_KEY);
+            String urlStr = builder.build().toString();
+            URL url = new URL(urlStr);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+
+            InputStream inputStream = httpURLConnection.getInputStream();
+            if (inputStream == null) {
+                return new ArrayList<>();
+            }
+            StringBuffer buffer = new StringBuffer();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                return new ArrayList<>();
+            }
+            jsonResults = buffer.toString();
+
+        } catch (IOException e) {
+
+            return new ArrayList<>();
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (final IOException e) {
+
+                }
+            }
+        }
+
+        ArrayList<Movie> movies = new ArrayList<>();
+        try {
+            JSONObject result = new JSONObject(jsonResults);
+            JSONArray moviesArray = result.getJSONArray(Constants.JSON_RESULTS);
+            for (int i = 0; i < moviesArray.length(); i++) {
+                movies.add(new Movie(moviesArray.getJSONObject(i)));
+            }
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+        return movies;
+    }
+
+    public interface MoviesApiCallResults {
+        void moviesApiCallResultsCallback(ArrayList<Movie> movies);
+    }
 }
+
+
