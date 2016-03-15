db242b648a9ee951030631a65f6a92e274bcb9f8
Anoop <optimistanoop@gmail.com>
2016-03-15 10:18:28 +0530

    mivie datail fragment binding view

---
+++ app/src/main/java/com/anoop/android/udacitypopularmovies/MovieDetailActivity.java
@@ -0,0 +1,44 @@
+package com.anoop.android.udacitypopularmovies;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.security.InvalidParameterException;
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setTitle("MovieDetail");
+        }
+
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            ViewCompat.setElevation(toolbar, 0);
+        }
+
+
+        if(savedInstanceState == null)
+        {
+            Movie movie = getIntent().getExtras().getParcelable(Constants.MOVIE);
+            if(movie == null)
+            {
+                throw new InvalidParameterException("Invalid parameter for movie detail.");
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.movieFrame, MovieDetailFragment.newInstance(movie))
+                    .commit();
+        }
+
+    }
+
+}
