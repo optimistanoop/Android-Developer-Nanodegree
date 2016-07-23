7e68867f1175f065203abc0cb26cb0ae712c6cbd
Anoop Kumar <optimistanoop@gmail.com>
2016-07-23 14:44:52 +0530

    Step 1: Create a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,10 +2,13 @@ package com.udacity.gradle.builditbigger;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.Joke;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,7 +43,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast toast = Toast.makeText(this, Joke.getJoke(), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 
 
