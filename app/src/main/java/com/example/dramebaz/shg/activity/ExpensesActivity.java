664c12d6feec3050178db530aa0559239842afca
Anoop Kumar <optimistanoop@gmail.com>
2016-09-04 14:39:49 +0530

    back btn impl

--- app/src/main/java/com/example/dramebaz/shg/activity/ExpensesActivity.java
+++ app/src/main/java/com/example/dramebaz/shg/activity/ExpensesActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -75,6 +76,9 @@ public class ExpensesActivity extends AppCompatActivity {
         // Handle item selection
 
         switch (item.getItemId()) {
+            case R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return  true;
             case R.id.addFrndToGrp:
                 addGroupMember(123,"baba","sai","***@gmail.com");
                 return true;
